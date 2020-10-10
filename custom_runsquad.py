515c0e8676e334b09a4d49725be42a02008fef93
LorenzoSimone <loughl@hotmail.it>
2020-10-10 16:23:48 +0200

    Plot update

--- custom_runsquad.py
+++ custom_runsquad.py
@@ -4,70 +4,25 @@ from data_prepocessing import add_token_positions, add_end_idx, read_squad
 from transformers import AutoTokenizer, ElectraForQuestionAnswering, ElectraConfig, AdamW, ElectraTokenizerFast
 from torch.utils.data import DataLoader
 from transformers import AdamW
-from run_squad import evaluate
 from types import SimpleNamespace
 import argparse
 import torch
 import itertools
 import os
+import json
+import subprocess
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
 
 tokenizer = ElectraTokenizerFast.from_pretrained('deepset/electra-base-squad2')
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+#device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+device = torch.device('cpu')
 EPOCHS = 3
 BATCH_SIZE = 16
 RESULTS_FOLDER = 'results'
-
-eval_args = {
-   "model_type":"electra",
-   "model_name_or_path":"deepset/electra-base-squad2",
-   "output_dir":"output",
-   "data_dir":"squad",
-   "train_file": None,
-   "predict_file":"valid_squadv2.json",
-   "config_name":"",
-   "tokenizer_name":"",
-   "cache_dir":"",
-   "version_2_with_negative": True,
-   "null_score_diff_threshold":0.0,
-   "max_seq_length":384,
-   "doc_stride":128,
-   "max_query_length":64,
-   "do_train":False,
-   "do_eval":True,
-   "evaluate_during_training":False,
-   "do_lower_case":True,
-   "per_gpu_train_batch_size":8,
-   "per_gpu_eval_batch_size":16,
-   "learning_rate":5e-05,
-   "gradient_accumulation_steps":1,
-   "weight_decay":0.0,
-   "adam_epsilon":1e-08,
-   "max_grad_norm":1.0,
-   "num_train_epochs":3.0,
-   "max_steps":-1,
-   "warmup_steps":0,
-   "n_best_size":20,
-   "max_answer_length":30,
-   "verbose_logging":False,
-   "lang_id":0,
-   "logging_steps":500,
-   "save_steps":500,
-   "eval_all_checkpoints":False,
-   "no_cuda":False,
-   "overwrite_output_dir":False,
-   "overwrite_cache":False,
-   "seed":42,
-   "local_rank":-1,
-   "fp16":False,
-   "fp16_opt_level":"O1",
-   "server_ip":"",
-   "server_port":"",
-   "threads":1,
-   "n_gpu":0,
-   "device": device
-}
-
-eval_args = SimpleNamespace(**eval_args)
+LOSS_FILE = 'losses.json'
+IMAGE_FOLDER = 'img'
 
 # Building the parser syntax 
 parser = argparse.ArgumentParser(description='Use me if you want load and preprocessing the custom squad data')
@@ -76,11 +31,15 @@ parser = argparse.ArgumentParser(description='Use me if you want load and prepro
 parser.add_argument('-lr', '--learning_rate', type=str, metavar='', required=True, help='insert the learning rate possible values')
 parser.add_argument('-hs', '--size_hidden', type=str, metavar='', required=True, help='insert the hidden size possible values')
 parser.add_argument('-hl', '--num_hidden_layer', type=str, metavar='', required=True, help='insert the number of hidden layers possible values')
+
 #File path parameters
 parser.add_argument('-path_tr_f', '--path_trainingData_file', type=str, metavar='', required=True, help='insert the path of training custom squad dataset')
 parser.add_argument('-path_vl_f', '--path_validationData_file', type=str, metavar='', required=True, help='insert the path of validation custom squad dataset')
 args = parser.parse_args()
 
+args.learning_rate = [eval(item) for item in args.learning_rate.split(',')]
+args.size_hidden = [eval(item) for item in args.size_hidden.split(',')]
+args.num_hidden_layer = [eval(item) for item in args.num_hidden_layer.split(',')]
 
 #Data preparation code
 train_contexts, train_questions, train_answers = read_squad(args.path_trainingData_file)
@@ -102,21 +61,25 @@ val_dataset = SquadDataset(val_encodings)
 
 
 #Constructing list of hyperparameters configurations
-hyperparameters = [eval(args.learning_rate), eval(args.size_hidden), eval(args.num_hidden_layer)]
+
+hyperparameters = [args.learning_rate, args.size_hidden, args.num_hidden_layer]
 configurations = [i for i in itertools.product(*hyperparameters)]
 
 j = 1
 
-res_path = '/{}'.format(RESULTS_FOLDER)
-try:
-  os.mkdir(res_path) 
-except FileExistsError:
-  print("Directory " , res_path ,  " already exists")
+res_path = './{}/{}'.format(RESULTS_FOLDER,LOSS_FILE)
+img_path = './{}/{}'.format(RESULTS_FOLDER,IMAGE_FOLDER)
+os.makedirs(os.path.dirname(res_path), exist_ok=True)
+os.mkdir(img_path)
+
+with open(res_path, "w") as f:
+    json.dump({'title': 'Grid search | Available Hyperparameters [learn_rate, hidden size, hidden layer] | {}'.format(configurations), 'results': []}, f)
 
 for conf in configurations:
 
-  conf_id = 'config{}_{}_{}_{}'.format(j, *conf)
+  conf_id = '{}_config_{}_{}_{}'.format(j, *conf)
   print('Run [{}] --learning rate: {}, --hidden size: {}, --hidden layer: {}'.format(j, *conf))
+
   # Initializing a ELECTRA electra-base-uncased style configuration
   electra_conf = ElectraConfig(hidden_size = conf[1], num_hidden_layers = conf[2])
   # Initializing a model from the electra-base-uncased style configuration
@@ -127,14 +90,14 @@ for conf in configurations:
   model.train()
 
   train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
   learning_rate = conf[0]
   optim = AdamW(model.parameters(), lr=learning_rate)
   loss_records = []
   bi = 0
-  for epoch in range(EPOCHS):
+
+  for epoch in tqdm(range(EPOCHS)):
     for batch in train_loader:
-        if(bi > 2):
+        if(bi > 3):
           break
         optim.zero_grad()
         input_ids = batch['input_ids'].to(device)
@@ -143,15 +106,24 @@ for conf in configurations:
         end_positions = batch['end_positions'].to(device)
         outputs = model(input_ids, attention_mask=attention_mask, start_positions=start_positions, end_positions=end_positions)
         loss = outputs[0]
-        loss_records.append(loss)
+        loss_records.append(loss.item())
         loss.backward()
         optim.step()
         bi+=1
 
-
-  model.eval()
-  print(evaluate(eval_args, model, tokenizer))
   model.save_pretrained(conf_id)
   tokenizer.save_pretrained(conf_id)
+
+  with open(res_path, "r") as f:
+    records = json.load(f)
+  with open(res_path, "w") as f:
+    new_rec = {'conf_id': conf_id, 'loss': loss_records}
+    json.dump({'title': records['title'], 'results': [*records['results'], new_rec]}, f)
+
+  plt.figure()
+  plt.plot(loss_records)
+  plt.title('Learning curve {}'.format(conf_id))
+  plt.savefig('./{}/{}.png'.format(img_path,conf_id))
+  plt.close()
 
   j += 1
